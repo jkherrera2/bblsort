8584a51218b38161ff9aaeefeb0000a0dd405789
John Kenneth Herrera <jeherrera@up.edu.ph>
2023-06-12 18:04:24 +0800

    Update bubbleSort.java

--- Sort/Bubble Sort/src/bubbleSort.java
+++ Sort/Bubble Sort/src/bubbleSort.java
@@ -2,15 +2,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class bubbleSort {
    public static void main(String[] args) throws Exception {
 
-		for(int trial = 1; trial <= 20; trial++){
+		for(int trial = 1; trial <= 3; trial++){
+			int[] array = readData();
+
 			long timeStart = System.nanoTime();
 
-			sort.bubbleSort();
+			//sort.bubbleSort();
+			sort.selectionSort(array);
+			//sort.insertionSort(array);
+			//sort.quickSort();
+			//Arrays.sort(array);
 
 			long timeEnd = System.nanoTime();
         	long runTime = timeEnd - timeStart;
@@ -40,7 +47,6 @@ public class bubbleSort {
             e.printStackTrace();
         }
 
-        // Convert the list to an array
         int[] dataset = new int[numbers.size()];
         for (int i = 0; i < numbers.size(); i++) {
             dataset[i] = numbers.get(i);
@@ -50,27 +56,61 @@ public class bubbleSort {
 	}
 
    	class sort {
-	static void bubbleSort(){
-		int[] array = readData();
-		int size = array.length;
+		static void bubbleSort(){
+			int[] array = readData();
+			int size = array.length;
 
-		// loop to access each array element
+			for (int i = 0; i < size - 1; i++){
 
-		for (int i = 0; i < size - 1; i++){
+				for (int j = 0; j < size - i - 1; j++){
 
-		// loop to compare array elements
-			for (int j = 0; j < size - i - 1; j++)
-
-				// compare two adjacent elements
-				if (array[j] > array[j + 1]) {
-
-				// swapping occurs if elements
-				// are not in the intended order
-				int temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+					if (array[j] > array[j + 1]) {
+						int temp = array[j];
+						array[j] = array[j + 1];
+						array[j + 1] = temp;
+					}
+				}
 			}
 		}
+
+		static void selectionSort(int[] array){
+			array = readData();
+			int size = array.length;
+
+			for (int step = 0; step < size - 1; step++) {
+      			int min_idx = step;
+
+      		for (int i = step + 1; i < size; i++) {
+        		if (array[i] < array[min_idx]) {
+          		min_idx = i;
+        		}
+      		}
+      		int temp = array[step];
+      		array[step] = array[min_idx];
+      		array[min_idx] = temp;
+    		}
 		}
+
+		static void insertionSort(int[] array){
+			array = readData();
+			int size = array.length;
+
+			for (int step = 1; step < size; step++) {
+      			int key = array[step];
+      			int j = step - 1;
+
+      			while (j >= 0 && key < array[j]) {
+        			array[j + 1] = array[j];
+        			--j;
+      			}
+				
+      		array[j + 1] = key;
+    		}
+    	}
+
+		// static void quickSort(){
+		// 	int[] array = readData();
+		// 	int size = array.length;
+		// }
 	}
 }
