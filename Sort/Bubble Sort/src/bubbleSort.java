9e771ce2588adc36a04bef5ca7fe756391906920
John Kenneth Herrera <jeherrera@up.edu.ph>
2023-06-14 19:17:37 +0800

    Update bubbleSort.java

--- Sort/Bubble Sort/src/bubbleSort.java
+++ Sort/Bubble Sort/src/bubbleSort.java
@@ -14,9 +14,9 @@ public class bubbleSort {
 			long timeStart = System.nanoTime();
 
 			//sort.bubbleSort();
-			sort.selectionSort(array);
-			//sort.insertionSort(array);
-			//sort.quickSort();
+			//sort.selectionSort(array);
+			sort.insertionSort(array);
+			//sort.quickSort(array, 0, array.length - 1);
 			//Arrays.sort(array);
 
 			long timeEnd = System.nanoTime();
@@ -108,9 +108,31 @@ public class bubbleSort {
     		}
     	}
 
-		// static void quickSort(){
-		// 	int[] array = readData();
-		// 	int size = array.length;
-		// }
+		static int partition(int array[], int low, int high) {
+    	int pivot = array[high];
+    	int i = (low - 1);
+
+    	for (int j = low; j < high; j++) {
+      		if (array[j] <= pivot) {
+        		i++;
+        		int temp = array[i];
+        		array[i] = array[j];
+        		array[j] = temp;
+      		}
+    	}
+
+    	int temp = array[i + 1];
+    	array[i + 1] = array[high];
+    	array[high] = temp;
+    	return (i + 1);
+  		}
+
+  		static void quickSort(int array[], int low, int high) {
+    		if (low < high) {
+				int pi = partition(array, low, high);
+      			quickSort(array, low, pi - 1);
+      			quickSort(array, pi + 1, high);
+    		}
+  		}		
 	}
 }
